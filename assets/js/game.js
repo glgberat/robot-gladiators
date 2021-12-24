46632c6f1f4e1daf147f521024b0b50ad4c1a6c0
Abbush <glgberat@yahoo.com>
2021-12-24 00:27:50 -0500

    UPDATED FOR LOOP

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,7 +14,8 @@ var enemyAttack = 12;
 
 var fight = function(enemyName) {
  
-  window.alert("Welcome to Robot Gladiators!");
+   // repeat and execute as long as the enemy-robot is alive 
+   while(enemyHealth > 0) {
 
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   console.log(promptFight);
@@ -66,6 +67,9 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   window.alert("You need to choose a valid option. Try again!");
 }
 }
-for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+}
+for (var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
